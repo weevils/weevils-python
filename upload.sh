a6e2679447122fa0f83a0e637977f34c2b6f2d37
Carl Crowder <carl@zerfeciz.com>
2023-04-23 13:37:51 +0700

    Updating upload script to use custom PyPI temporarily until it becomes stable enough to put on GitHub and public PyPI

--- upload.sh
+++ upload.sh
@@ -6,9 +6,9 @@ chmod 0600 key
 
 rm -rf dist/
 
-sed -i "s/version = \"\(.*\)\"/version = \"\1-$(date +%Y%m%d%H%M)\"/" pyproject.toml
+sed -i "0,/version/{s/version = \"\(.*\)\"/version = \"\1.$(date +%Y%m%d%H%M)\"/}" pyproject.toml
 /venv/bin/poetry build
 
 # TODO: host key checking...
-ssh -o StrictHostKeyChecking=no -i key pypi@pypi.zerfeciz.com mkdir -p packages/weevils-python/
-scp -o StrictHostKeyChecking=no -i key dist/* pypi@pypi.zerfeciz.com:packages/weevils-python/
+ssh -o StrictHostKeyChecking=no -i key pypi@pypi.zerfeciz.com mkdir -p packages/weevils/
+scp -o StrictHostKeyChecking=no -i key dist/* pypi@pypi.zerfeciz.com:packages/weevils/
